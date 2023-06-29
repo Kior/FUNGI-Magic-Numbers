7f57f498bc33d49ceccf01965d915e7c5cabc7fa
Kior <kior.elan@gmail.com>
2023-06-29 05:19:10 +0300

    Turns, reset
    Counting turns, resetting values

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -7,6 +7,8 @@ public class MagicNumbers : MonoBehaviour
     private int _min;
     private int _max;
     private int _guess;
+    private int _press;
+
     private void Start()
     {
         _min = 1;
@@ -27,20 +29,28 @@ public class MagicNumbers : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             _max = _guess;
+            _press++;
             CalculateGuess();
             AskAboutGuess();
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             _min = _guess;
+            _press++;
             CalculateGuess();
             AskAboutGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log($"Поздравляю! Я угадал! Твоё число {_guess}");
+            Debug.Log($"Сделано {_press} ходов!");
+            _min = 1;
+            _max = 1000;
+            _guess = 0;
+            Debug.Log($"Значения min и max, guess сброшены до {_min} и {_max}, {_guess} соответственно.");
         }
     }
+    
 
     private void CalculateGuess()
     {
